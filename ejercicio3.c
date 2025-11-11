a25edc97820f659e8ea8a250812a2a120da2b9f5
Mariana-kiki <acosmariana9@outlook.com>
2025-11-11 11:53:00 -0600

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,17 @@
+/*Mariana Gutiérrez Acosta
+Tarea 2 ejercicio 3*/
+
+#include <stdio.h>
+
+void main() 
+{
+    int dolares;
+    int pesos;
+
+    printf("Ingrese la cantidad en dólares: ");
+    scanf("%d", &dolares);
+
+    pesos = dolares * 11.96;
+    
+    printf("Equivalente en pesos: %d\n", pesos);
+}
