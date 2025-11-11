36fb51b74068113ac92efb1d4000c622398e2844
Mariana-kiki <acosmariana9@outlook.com>
2025-11-11 12:16:43 -0600

    Create ejercicio7.c

---
+++ ejercicio7.c
@@ -0,0 +1,25 @@
+/*Mariana Gutiérrez Acosta
+Tarea 2 ejercicio 7*/
+
+#include <stdio.h>
+#define PrecioxKm 0.23 
+
+void main() 
+{
+    int distancia_ida, dias_estancia;
+    float distancia_total, precio, descuento = 0;
+    int precio_final;
+    printf("Ingrese la distancia de ida (en km): ");
+    scanf("%d", &distancia_ida);
+    printf("Ingrese el número de días de estancia: ");
+    scanf("%d", &dias_estancia);
+    distancia_total = distancia_ida * 2;
+    precio = distancia_total * PrecioxKm;
+
+    if (dias_estancia > 7 && distancia_total > 800) 
+    {
+        descuento = precio * 0.30;
+    }
+    precio_final = (int)(precio - descuento);  
+    printf("El precio final del ticket es: $%d\n", precio_final);
+}
