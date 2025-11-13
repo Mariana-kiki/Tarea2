aacb37ccffab259786f1fb1ea6c18e9ca03366e1
Mariana-kiki <acosmariana9@outlook.com>
2025-11-13 08:34:20 -0600

    Create ejercicio9.c

---
+++ ejercicio9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() 
+{
+    int N, i;
+
+    printf("Ingrese un número entero: ");
+    scanf("%d", &N);
+    printf("Tabla de multiplicar del %d:\n", N);
+    
+    for (i = 1; i <= 10; i++) 
+    {
+        printf("%d x %d = %d\n", N, i, N * i);
+    }
+}
