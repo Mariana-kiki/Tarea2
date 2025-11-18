721c4c6265aec9e25f97da03429bb373709d6e99
Mariana-kiki <acosmariana9@outlook.com>
2025-11-18 08:33:52 -0600

    Create ejercicio13.c

---
+++ ejercicio13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() 
+{
+    int n, i;
+    int suma = 0;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    int arreglo[n]; 
+  
+    for(i = 0; i < n; i++) 
+    {
+        printf("Ingrese el elemento %d: ", i+1);
+        scanf("%d", &arreglo[i]);
+        suma = suma + arreglo[i]; 
+    }
+    printf("La suma de los elementos es: %d\n", suma);
+}
