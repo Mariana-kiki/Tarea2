fe24bacdde6931c25bba01409d201209eb78d637
Mariana-kiki <acosmariana9@outlook.com>
2025-11-18 08:41:17 -0600

    Create ejercicio14.c

---
+++ ejercicio14.c
@@ -0,0 +1,27 @@
+/*Mariana Gutiérrez Acosta
+Tarea 2 ejercicio 14*/
+
+#include <stdio.h>
+void main() 
+{
+    int n, i, x, contador = 0;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    int arreglo[n]; // Declaración del arreglo
+
+    for(i = 0; i < n; i++) 
+    {
+        printf("Ingrese el elemento %d: ", i+1);
+        scanf("%d", &arreglo[i]);
+    }
+    printf("Ingrese el número a buscar: ");
+    scanf("%d", &x);
+    for(i = 0; i < n; i++) 
+    {
+        if(arreglo[i] == x) 
+        {
+            contador++;
+        }
+    }
+    printf("El número %d aparece %d veces en el arreglo.\n", x, contador);
+}
