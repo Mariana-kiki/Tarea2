9b84ab66b1cde43246b0fbce88fe142b1bfcb0bc
Mariana-kiki <acosmariana9@outlook.com>
2025-11-19 21:35:53 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -2,19 +2,21 @@
 Tarea 2 ejercicio 2*/
 
 #include <stdio.h>
-#define PI 3.1416 
 
 void main() 
 {
-    int radio;
-    int area, circunferencia;
+    int radio, area, circunferencia;
 
     printf("Ingrese el radio del círculo: ");
     scanf("%d", &radio);
 
-    area = PI * radio * radio;
-    circunferencia = 2 * PI * radio;
-
+    while (radio < 0) 
+    {
+        printf("El radio no puede ser negativo. Intentalo de nuevo: ");
+        scanf("%d", &radio);
+    }
+    area = 3.1416 * radio * radio;         
+    circunferencia = 2 * 3.1416 * radio;    
     printf("\nResultados:\n");
     printf("Área del círculo: %d\n", area);
     printf("Circunferencia: %d\n", circunferencia);
